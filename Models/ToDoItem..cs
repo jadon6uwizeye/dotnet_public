0e6a15c8d6138bf868fa70a921cb844eb6d4ec66
Uwizeye Jean de Dieu <jeandedieuuwizeye6@gmail.com>
2023-03-22 12:22:56 +0200

    progress

--- Models/ToDoItem..cs
+++
@@ -1,8 +0,0 @@
-namespace MyWebAPI.models
-{
-    public class TodoItem{
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public bool IsComplete { get; set; }
-    }
-}
